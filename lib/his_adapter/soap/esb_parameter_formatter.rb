a6867b543f0c8d5df900544600e9f91557f87fa2
ian <ianlynxk@gmail.com>
2021-11-24 17:27:33 +0800

    修改 esb 参数格式化的 bug

--- lib/his_adapter/soap/esb_parameter_formatter.rb
+++ lib/his_adapter/soap/esb_parameter_formatter.rb
@@ -18,13 +18,33 @@ module HisAdapter
       end
 
       def format
-        xml_params = Soap::ParameterFormatter.new(api,
-                                                  origin_params,
-                                                  adapter: adapter,
-                                                  xml_root: xml_root,
-                                                  wrap_field: wrap_field).format
+        params = convert_field(origin_params)
+        xml_params = params_to_xml(params)
+
 
         wrap_esb_text(xml_params)
+      end
+
+      def params_to_xml(params)
+        return params if params.nil?
+
+        # 因为有些服务不需要根标签，这时候只要传入值为 "" 的 xml_root 即可。
+        # 因为 to_xml 的 root 参数为 "" 会产生一对 <></> 空标签作为根标签，这里将这对空标签去除
+        if xml_root == ""
+          params.to_xml(
+            root: @xml_root,
+            skip_instruct: true,
+            dasherize: false
+          )
+                .gsub("<>\n  ", "")
+                .gsub("\n</>\n", "")
+        else
+          params.to_xml(
+            root: @xml_root,
+            skip_instruct: true,
+            dasherize: false
+          )
+        end
       end
 
       def wrap_esb_text(xml_params)
